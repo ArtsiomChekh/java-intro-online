cbc7441ec7692320a72520b7a0914a174544aad3
ArtsiomChekh <qbartem@gmail.com>
2023-01-02 21:28:47 +0300

    commit task

---
+++ src/algorithmization/one_dimensional_array/Task10.java
@@ -0,0 +1,40 @@
+package algorithmization.one_dimensional_array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*
+  Дан целочисленный массив с количеством элементов n. Сжать массив, выбросив из него каждый второй
+  элемент (освободившиеся элементы заполнить нулями). Примечание. Дополнительный массив не использовать.
+*/
+public class Task10 {
+
+  public static void main(String[] args) {
+
+    try {
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("Введите значение для числа n:");
+      int n = scanner.nextInt();
+
+      int[] array = new int[n];
+
+      // заполним массив рандомными целыми числами
+      for (int i = 0; i < array.length; i++) {
+        array[i] = ((int) (Math.random() * 35) - 10);
+      }
+
+      System.out.println(Arrays.toString(array));
+
+      // заполним каждый второй элемент массива нулями
+      for (int i = 1; i < array.length; i += 2) {
+        array[i] = 0;
+      }
+
+      System.out.println(Arrays.toString(array));
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+}
