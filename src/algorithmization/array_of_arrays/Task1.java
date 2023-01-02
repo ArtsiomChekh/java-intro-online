608cc080cf0fd8d4846cac67e580706e0d834b37
ArtsiomChekh <qbartem@gmail.com>
2023-01-02 22:52:25 +0300

    commit task

---
+++ src/algorithmization/array_of_arrays/Task1.java
@@ -0,0 +1,43 @@
+package algorithmization.array_of_arrays;
+/*
+  Дана матрица. Вывести на экран все нечетные столбцы, у которых первый элемент больше последнего.
+*/
+
+public class Task1 {
+
+  public static void main(String[] args) {
+
+    try {
+      // создадим и заполним произвольно матрицу
+      int[][] matrix = {
+          {2, 4, 8, 7, 1},
+          {4, 66, 123, 12, 1},
+          {24, 32, 12, 12, 23},
+          {23, 42, 24, 1, 4},
+          {1, 4, 7, 2, 1}
+      };
+      // выведем на экран
+      for (int i = 0; i < matrix.length; i++) {
+        for (int j = 0; j < matrix[i].length; j++) {
+          System.out.printf("%4d", matrix[i][j]);
+        }
+        System.out.println();
+      }
+
+      System.out.println();
+
+      // выполним условие задания
+      for (int i = 0; i < matrix.length; i++) {
+        for (int j = 0; j < matrix[i].length; j += 2) {
+          if (matrix[0][j] > matrix[matrix.length - 1][j]) {
+            System.out.printf("%4d", matrix[i][j]);
+          }
+        }
+        System.out.println();
+      }
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
