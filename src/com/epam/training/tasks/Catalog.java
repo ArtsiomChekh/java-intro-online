2b61a4339e6bb1d9e86a715ee6f147c3bdbf1b70
ArtsiomChekh <qbartem@gmail.com>
2023-03-04 22:47:16 +0300

    commit catalog

---
+++ src/com/epam/training/tasks/Catalog.java
@@ -0,0 +1,30 @@
+package com.epam.training.tasks;
+
+import java.util.List;
+
+public class Catalog {
+
+  private User user;
+  private List<Book> books;
+
+  public Catalog(User user, List<Book> books) {
+    this.user = user;
+    this.books = books;
+  }
+
+  public User getUser() {
+    return user;
+  }
+
+  public void setUser(User user) {
+    this.user = user;
+  }
+
+  public List<Book> getBooks() {
+    return books;
+  }
+
+  public void setBooks(List<Book> books) {
+    this.books = books;
+  }
+}
